ba62fe5e227ab47c477dbeb9195b599b55860f3f
NixFester <abi00kun@gmail.com>
2024-12-23 14:12:30 +0700

    fix search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,16 +1,23 @@
 'use client'
-import React from "react";
+import React, { Suspense } from "react";
 import MainLayout from "../component/layout/MainLayout";
 import { useSearchParams } from "next/navigation";
 
-
-export default function SearchResultPage() {
+function SearchResultContent() {
   const searchParams = useSearchParams();
   const paramValue = searchParams.get('q');
-  
+
   return (
     <div>
       <MainLayout Kategori={4} query={paramValue || ""} />
     </div>
+  );
+}
+
+export default function SearchResultPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchResultContent />
+    </Suspense>
   );
 }
