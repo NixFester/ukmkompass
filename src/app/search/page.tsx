e2ffd4bb5341abefefe6313ad94631abd1d71c0d
NixFester <abi00kun@gmail.com>
2024-12-23 12:48:26 +0700

    renamed:    src/app/favico.ico -> src/app/favicon.ico 	modified:   src/app/search/page.tsx

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 import React from "react";
 import MainLayout from "../component/layout/MainLayout";
-import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
 export interface ISearchProps {}
 
 export default function SearchResultPage(props: ISearchProps) {
-  const router = useRouter();
-  const searchParams = new URLSearchParams(window.location.search);
+  const searchParams = useSearchParams();
   const paramValue = searchParams.get('q');
+  
   return (
     <div>
       <MainLayout Kategori={4} query={paramValue || ""} />
